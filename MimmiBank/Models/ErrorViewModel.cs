0b8ccb7163717fad38f6fffae06fa38fd653ddae
Mimmi Rundlöf <Mimmi.rundlof@yh.nackademin.se>
2018-11-07 17:39:31 +0100

    Add project files.

---
+++ MimmiBank/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MimmiBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
